4214d39052f11daa40a3a8b32a24598ec03ef537
Arut Thanomwatana <arut.tha@gmail.com>
2018-10-24 16:15:20 +0700

    Initialize internshop project

---
+++ src/main/java/com/sandstorm/internshop/InternshopApplication.java
@@ -0,0 +1,12 @@
+package com.sandstorm.internshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshopApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sandstorm/internshop/InternshopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sandstorm.internshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternshopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
